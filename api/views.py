506665a91ea81447550221789b4aafa3590c46f0
Dmytry <donchykd@gmail.com>
2023-05-28 23:02:35 +0300

    Adding views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Place
+from api.serializers import PlaceSerializer
 
-# Create your views here.
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
